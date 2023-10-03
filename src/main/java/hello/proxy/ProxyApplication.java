6a0866b4b64dfcd4dbff850d74003a04789dff94
hongsh429 <shh0429@kakao.com>
2023-10-04 08:58:23 +0900

    버전1 - 모든 layer를 인터페이스를 만들고 구현체를 만들어 수동으로 빈 등록하여 진행. --- @Import() - 클래스를 스프링 빈으로 등록한다. -> = AnnotatainConfig~~~(AppV1Config.class); 다양한 버전을 사용하기 위해서 사용한 것이다.

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,13 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") // 확인하자!
 public class ProxyApplication {
 
     public static void main(String[] args) {
