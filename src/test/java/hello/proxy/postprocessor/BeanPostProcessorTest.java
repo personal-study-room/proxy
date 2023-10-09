81aeb2e9cd0d5cf0907cf4f931d1ba60cc3f6f3d
hongsh429 <shh0429@kakao.com>
2023-10-09 09:47:50 +0900

    빈 후처리를 통한 component scan 대상들까지도 proxy 적용하기  - 단순히 component scan 대상도 proxy 생성 가능  - 수동 빈 등록 코드도 깔끔해진다.(매번 proxy를 반환하도록 bean을 만들지 않아도 된다.) --- 현재 package name 으로 proxy 만들 것인지 판단하는 중. 이를 pointcut 으로 대체하면 더 좋을 것이라 판단.

---
+++ src/test/java/hello/proxy/postprocessor/BeanPostProcessorTest.java
@@ -0,0 +1,69 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BeanPostProcessorTest {
+
+    @Test
+    void basicConfig() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(BeanPostProcessorConfig.class);
+
+        // A -> B 변경. B는 빈으로 등록된다.
+        B a = ac.getBean("beanA", B.class);
+        a.helloB();
+
+        // B는 빈으로 등록되지 않는다.
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean(A.class));
+    }
+
+    @Slf4j
+    @Configuration
+    static class BeanPostProcessorConfig {
+        @Bean("beanA")
+        public A a() {
+            return new A();
+        }
+
+        @Bean
+        public AToBPostProcessor helloPostProcessor() {
+            return new AToBPostProcessor();
+        }
+    }
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+
+    }
+
+    @Slf4j
+    static class AToBPostProcessor implements BeanPostProcessor {
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            log.info("beanName={}, bean={}", beanName, bean);
+            if (bean instanceof A) {
+                return new B();
+            }
+            return bean;
+        }
+    }
+}
